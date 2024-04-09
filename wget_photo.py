69fbf9a5b697e5f19f9f4cf44e056ca1b7453513
Will Shen <zwshen0603@gmail.com>
2024-04-09 08:21:49 +0800

    Update wget_photo.py

--- wget_photo.py
+++ wget_photo.py
@@ -1,16 +1,14 @@
 import subprocess
 
-# URL = "http://www.yung-chi.com/data/1091228%E6%88%B6%E5%A4%96%E5%90%91%E6%97%A5%20({0}).JPG"
-# URL = "http://www.yung-chi.com/data01/10911%E5%90%91%E6%97%A5%E8%91%B5%E7%B6%B2%E8%B7%AF%20({0}).JPG"
-# URL = "http://www.yung-chi.com/data01/109%E5%B9%B410%E6%9C%88%E5%90%91%20({0}).JPG"
-# URL = "http://www.yung-chi.com/data01/10909%E5%90%91%E6%97%A5%E5%B0%8F%E5%B9%BC%20({0}).JPG"
-URL = "http://www.yung-chi.com/data01/10908%E5%90%91%20({0}).JPG"
+# URL = "http://www.yung-chi.com/data01/10908%E5%90%91%20({0}).JPG"
 
-URL = "https://fapello.com/content/-/k/2km-2km-dj-miu-korean/1000/2km-2km-dj-miu-korean_0{0:03d}.jpg"
-# URL = "https://fapello.com/content/e/u/eunji-pyoapple/1000/eunji-pyoapple_0{0}.jpg" # 742 - 785
+# URL = "https://fapello.com/content/-/k/2km-2km-dj-miu-korean/1000/2km-2km-dj-miu-korean_0{0:03d}.jpg"
+# URL = "https://fapello.com/content/e/u/eunji-pyoapple/1000/eunji-pyoapple_0{0}.jpg" # 886 - 951
 # URL = "https://fapello.com/content/a/u/auddk/1000/auddk_0{0}.jpg"
-URL = "https://fapello.com/content/a/u/auddk-77/1000/auddk-77_{0:04d}.jpg" # 1 - 247
+URL = "https://fapello.com/content/a/u/auddk-77/1000/auddk-77_{0:04d}.jpg" # 247 - 459
+URL = "https://fapello.com/content/a/u/auddk/1000/auddk_{0:04d}.jpg" # 313 - 459
 # URL = "https://fapello.com/content/-/k/2km-2km-dj-miu-korean/1000/2km-2km-dj-miu-korean_0{0:03d}.jpg" # 391 to 433
+URL = "https://i0.wp.com/file.cosblay.com/wp-content/uploads/2023/07/30/3619730163741{0}.webp"
 
 def runcmd(cmd, verbose = False, *args, **kwargs):
     process = subprocess.Popen(
@@ -24,7 +22,6 @@ def runcmd(cmd, verbose = False, *args, **kwargs):
     if verbose:
         print(std_out.strip(), std_err)
 
-# for i in range(743, 786):
-for i in range(1, 248):
-    cmd = "wget "
+for i in range(1, 502):
+    cmd = "wget -U 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1 Safari/605.1.15' "
     runcmd( cmd + URL.format(i), verbose = True)
